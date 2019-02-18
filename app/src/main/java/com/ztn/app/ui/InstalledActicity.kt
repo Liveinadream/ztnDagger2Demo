ae821607f5ca13e4c3ba74169dfd2233176ec0af
张天宁 <zhangtianning@bjknrt.com>
2019-02-18 10:27:39 +0800

    准备加入本地应用列表与本地文件查看两个功能

---
+++ app/src/main/java/com/ztn/app/ui/InstalledActicity.kt
@@ -0,0 +1,17 @@
+package com.ztn.app.ui
+
+import com.ztn.app.base.SimpleActivity
+
+/**
+ * Created by 冒险者ztn on 2019/2/18.
+ * 介绍 todo
+ */
+class InstalledActicity:SimpleActivity(){
+    override fun getLayout(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun initEventAndData() {
+    }
+
+}
