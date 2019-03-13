dbdc94be89bbd8794d0b849280d92e264b5f63a4
张天宁 <zhangtianning@bjknrt.com>
2019-03-13 16:04:09 +0800

    删除一些没有必要的代码

--- app/src/main/java/com/ztn/app/ui/SettingActivity.kt
+++ app/src/main/java/com/ztn/app/ui/SettingActivity.kt
@@ -1,9 +1,7 @@
 package com.ztn.app.ui
 
 import android.os.Bundle
-import android.util.Log
 import com.ztn.app.R
-import com.ztn.app.adapter.FileAdapterEnum
 import com.ztn.app.base.SimpleActivity
 import com.ztn.app.fragment.setting.LogoSettings
 
@@ -23,38 +21,6 @@ class SettingActivity : SimpleActivity() {
         super.onCreate(savedInstanceState)
 
         supportFragmentManager.beginTransaction().replace(R.id.content, LogoSettings()).commit()
-
-
-        val a = add({ add(1, 2) }, { 2 + 3 })
-
-        Log.d(
-            "得到的adapter数量：", "${FileAdapterEnum.INSTANCE.getNum()}"
-        )
-    }
-
-//    tailrec fun findFixPoint(x: Double = 1.0): Double = if (x == Math.cos(x)) x else findFixPoint(Math.cos(x))
-
-    private inline fun add(sumOne: () -> Int, sumTwo: () -> Int): Int {
-        return sumOne.invoke() + sumTwo.invoke()
-    }
-
-    private inline fun fun2(body: () -> Unit) {
-
-    }
-
-    private fun add(sumOne: Int, sumTwo: Int): Int {
-        return sumOne + sumTwo
-    }
-
-    private fun test() {
-        var a = 1
-        var b = 5
-
-        if (a >= b) {
-            add({ add(a, b) }, { b })
-        } else {
-            add(b, a)
-        }
     }
 
 }
